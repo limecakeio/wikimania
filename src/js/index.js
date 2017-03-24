2d1970eb69ede17dc6e73f70a00bb8c1655529ea
1520152NPHSM <nicolas.1.pilch@gmail.com>
2017-03-24 16:41:46 +0100

    updated backend

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,43 @@
 angular.module('wikimania',[]).controller('wikimania-controller', function ($scope, $timeout, $window, $http) {
 
-  var self = this;
-	$scope.articeHTML = '';
-  $scope.title = '';
-	$scope.getArticle = function(page) {
+  $scope.activeArticle = {
+    articeHTML: '',
+    title: '',
+    id: -1
+  };
+
+  $scope.articleArchive = [];
+
+  let endArticle = {
+    title: '',
+    html: '',
+    id: -1
+  };
+
+  $scope.startGame = function(startID, endID) {
+    console.log(IDToTitle(startID));
+    return;
+    activeArticle = getArticle(IDToTitle(startID));
+    $scope.articleArchive.push(activeArticle);
+    endArticle = getArticle(IDToTitle(endID));
+  };
+
+	const getArticle = function(page) {
+    const article = {
+        title: 'Invalid',
+        html: 'Invalid',
+        id: -1
+    };
+
     $http({
 	      method: 'GET',
         url: 'https://de.wikipedia.org/w/api.php?origin=*&action=parse&prop=text&format=json&page=' + page,
     }).then(function successCallback(response) {
-      console.log(JSON.stringify(response.data).indexOf('update'));
-      $scope.articeHTML = parse(response.data.parse.text["*"]);
+      let context = response.data.parse;
+      article.title = context.title;
+      article.id = context.pageid;
+      article.articeHTML = parse(context.text["*"]);
+      return article;
     }, function errorCallback(response) {
       console.log('something went wrong');
     });
@@ -72,8 +100,32 @@ angular.module('wikimania',[]).controller('wikimania-controller', function ($sco
       audio.outerHTML = '';
     }
 
-//    document.querySelector('#contextNode').append(wikiContainer);
     return wikiContainer.innerHTML;
+  };
+
+  const IDToTitle = function(id) {
+    console.log('https://de.wikipedia.org/w/api.php?origin=*&action=query&pageids=' + id);
+    $http({
+	      method: 'GET',
+        url: 'https://de.wikipedia.org/w/api.php?origin=*&action=query&format=json&pageids=' + id
+    }).then(function successCallback(response) {
+      let context = Object.keys(response.data.query.pages);
+      console.log(context);
+      return context['title'];
+    }, function errorCallback(response) {
+      console.log('something went wrong');
+    });
+  };
+
+  const titleToID = function(title) {
+    $http({
+        method: 'GET',
+        url: 'https://de.wikipedia.org/w/api.php?origin=*&action=parse&prop=text&format=json&page=' + page
+    }).then(function successCallback(response) {
+      return response.data.parse.pageid;
+    }, function errorCallback(response) {
+      console.log('something went wrong');
+    });
   };
 
 });
