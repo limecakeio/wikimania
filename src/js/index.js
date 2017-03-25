56388187f2a16c57aec41a349ff9f3cdf200ce33
1520152NPHSM <nicolas.1.pilch@gmail.com>
2017-03-25 19:23:03 +0100

    backend update

--- src/js/index.js
+++ src/js/index.js
@@ -1,62 +1,89 @@
 angular.module('wikimania',[]).controller('wikimania-controller', function ($scope, $timeout, $window, $http) {
 
   $scope.activeArticle = {
-    articeHTML: '',
+    html: '',
     title: '',
-    id: -1
+    id: ''
   };
+
+  $scope.articleCounter = 0;
+
+  $scope.goalReached = false;
 
   $scope.articleArchive = [];
 
   let endArticle = {
     title: '',
     html: '',
+    preview: '',
     id: -1
   };
 
+  let difficultiyLevel = 0;
+
+  /*
+   * sets the activeArticle object with the argument startID
+   * endArticle is saved internally
+   */
   $scope.startGame = function(startID, endID) {
-    console.log(IDToTitle(startID));
-    return;
-    activeArticle = getArticle(IDToTitle(startID));
-    $scope.articleArchive.push(activeArticle);
-    endArticle = getArticle(IDToTitle(endID));
+    IDToTitle(startID).then(title => {
+      getArticle(title).then(article => {
+        $scope.activeArticle = article;
+        $scope.articleArchive.push(article);
+        document.querySelector('#contextNode').innerHTML = $scope.activeArticle.html;
+      });
+    });
+    IDToTitle(endID).then(title => {
+      getArticle(title).then(article => {
+        endArticle = article;
+      });
+    });
   };
 
 	const getArticle = function(page) {
-    const article = {
-        title: 'Invalid',
-        html: 'Invalid',
-        id: -1
-    };
-
-    $http({
+    return $http({
 	      method: 'GET',
-        url: 'https://de.wikipedia.org/w/api.php?origin=*&action=parse&prop=text&format=json&page=' + page,
+        url: 'https://de.wikipedia.org/w/api.php?origin=*&action=parse&prop=text|images&format=json&page=' + page,
     }).then(function successCallback(response) {
       let context = response.data.parse;
-      article.title = context.title;
-      article.id = context.pageid;
-      article.articeHTML = parse(context.text["*"]);
-      return article;
+      return {
+        title: context.title,
+        id: context.pageid,
+        html: parse(context.text['*'])
+      };
     }, function errorCallback(response) {
       console.log('something went wrong');
     });
-
   };
 
+  /*
+   *
+   */
   const parse = text => {
     let wikiContainer = document.createElement('div');
     wikiContainer.innerHTML = text;
 
+/*    let anchors = wikiContainer.querySelectorAll('a');
+    anchors.forEach(anchor => {
+  //    console.log(anchor.innerHTML);
+      anchor.addEventListener('click', clickHandler);
+    });*/
+
     let images = wikiContainer.querySelectorAll('img');
     images.forEach(image => {
+      image.src = '';
+      image.srcset = '';
       image.outerHTML = '';
     });
 
     let thumbImages = wikiContainer.querySelectorAll('.thumbimage');
     thumbImages.forEach(image => {
-      console.log(image);
       image.outerHTML = '';
+    });
+
+    let unwantedReferences = wikiContainer.querySelectorAll('.reference');
+    unwantedReferences.forEach(reference => {
+      reference.outerHTML = '';
     });
 
     let spans = wikiContainer.querySelectorAll('span');
@@ -74,7 +101,7 @@ angular.module('wikimania',[]).controller('wikimania-controller', function ($sco
     const unwantedLists = ['#Siehe_auch', '#Literatur', '#Weblinks', '#Einzelnachweise'];
 		unwantedLists.forEach(list => {
       let header = wikiContainer.querySelector(list);
-		    if (typeof header !== 'undefined') {
+		    if (typeof header !== 'undefined' && header !== null) {
           let parent = header.parentElement;
           let sibling = parent.nextSibling;
           while (sibling.nodeName === '#text') {
@@ -85,7 +112,7 @@ angular.module('wikimania',[]).controller('wikimania-controller', function ($sco
       }
     });
 
-    const unwantedElements = ['a', 'ul', 'div', 'img'];
+    const unwantedElements = [/*'a',*/ 'ul', 'div', 'img'];
 		unwantedElements.forEach(element => {
       const elementContainer = wikiContainer.querySelectorAll(element);
 			elementContainer.forEach(ele => {
@@ -95,37 +122,135 @@ angular.module('wikimania',[]).controller('wikimania-controller', function ($sco
       });
     });
 
+    let unwantedDefinitionBox = wikiContainer.querySelector('#Vorlage_Begriffsklaerung');
+    if (unwantedDefinitionBox !== null) {
+      unwantedDefinitionBox.outerHTML = '';
+    }
+
     let audio = wikiContainer.querySelector('#Vorlage_Gesprochene_Version');
-    if (typeof audio !== 'undefined') {
+//    if (typeof audio !== 'undefined') {
+    if (audio !== null) {
       audio.outerHTML = '';
+    }
+
+    let unwantedEmpty = wikiContainer.querySelector('#Vorlage_Lueckenhaft');
+    if (unwantedEmpty !== null) {
+      unwantedEmpty.outerHTML = '';
     }
 
     return wikiContainer.innerHTML;
   };
 
   const IDToTitle = function(id) {
-    console.log('https://de.wikipedia.org/w/api.php?origin=*&action=query&pageids=' + id);
-    $http({
+    return $http({
 	      method: 'GET',
         url: 'https://de.wikipedia.org/w/api.php?origin=*&action=query&format=json&pageids=' + id
     }).then(function successCallback(response) {
-      let context = Object.keys(response.data.query.pages);
-      console.log(context);
-      return context['title'];
+      return response.data.query.pages['' + id].title;
     }, function errorCallback(response) {
       console.log('something went wrong');
+      return 'err';
     });
   };
 
   const titleToID = function(title) {
-    $http({
+    return $http({
         method: 'GET',
-        url: 'https://de.wikipedia.org/w/api.php?origin=*&action=parse&prop=text&format=json&page=' + page
+        url: 'https://de.wikipedia.org/w/api.php?origin=*&action=parse&prop=text&format=json&page=' + title
     }).then(function successCallback(response) {
       return response.data.parse.pageid;
     }, function errorCallback(response) {
       console.log('something went wrong');
+      return 'something went wrong';
     });
   };
 
+  $scope.setLevel = function(diviculty) {
+
+  };
+
+  const checkArticle = function() {
+    if ($scope.articleArchive[$scope.articleArchive.length - 1].id === endArticle.id) {
+      $scope.goalReached = true;
+      console.log('fin');
+    }
+  };
+
+  const loadNewArticle = function(page) {
+    getArticle(page).then(article => {
+      $scope.articleArchive.push(article);
+      $scope.activeArticle = article;
+      document.querySelector('#contextNode').innerHTML = article.html;
+      checkArticle();
+    })
+//    articleArchive.push(getArticle(page));
+//    checkArticle();
+  };
+
+  $scope.getRandomArticle = function() {
+    getRandomArticleTitle().then(title => {
+      getArticle(title).then(article => {
+        $scope.articleArchive.push(article);
+        $scope.activeArticle = article;
+        document.querySelector('#contextNode').innerHTML = article.html;
+      });
+    });
+  };
+
+  const getRandomArticleTitle = function() {
+    return $http({
+        method: 'GET',
+        url: 'https://de.wikipedia.org/w/api.php?origin=*&action=query&list=random&rnnamespace=0&format=json'
+    }).then(response => {
+      return response.data.query.random[0].title;
+    }, err => {
+      console.log('random article failed');
+      return 'something went wrong';
+    });
+  };
+
+  const clickHandler = event => {
+      event.preventDefault();
+  };
+
+  $scope.getPreview = (selector) => {
+    if (endArticle.id === -1) {
+      throw new Error('no goal!');
+    }
+    $http({
+        method: 'GET',
+        url: 'https://de.wikipedia.org/w/api.php?origin=*&format=json&action=query&prop=pageterms|extracts&exintro=&explaintext=&titles=' + endArticle.title
+    }).then(response => {
+      endArticle.preview = response.data.query.pages['' + endArticle.id].extract;
+      document.querySelector('#' + selector).innerHTML = endArticle.preview;
+    }, err => {
+      console.log('random article failed');
+      return 'something went wrong';
+    });
+  };
+
+  let clickEventHandler = event => {
+    if (event.target.tagName === 'a' || event.target.tagName === 'A') {
+      let href = event.target.href;
+      if (href.indexOf('action=') == -1 && href.indexOf('(Seite nicht vorhanden)') == -1){
+        let titleArray = event.target.href.split('/');
+        loadNewArticle(titleArray[titleArray.length - 1]);
+      }
+      event.preventDefault();
+    }
+  };
+
+  if (document.addEventListener) {
+      document.addEventListener('click', clickEventHandler);
+  } else if (document.attachEvent) {
+      document.attachEvent('onclick', clickEventHandler);
+  }
+
 });
+
+
+// document.addEventListener('contextmenu', event => event.preventDefault());
+
+/*
+ * startGame(287816, 2166425) -> Kulturapfel -> Hessen
+ */
